1d46f0da8707127f9ff39441a442bd3d6a5de2d3
rafaelrotto <rafaelrotto@gmail.com>
2022-12-04 21:40:02 -0300

    first commit

---
+++ src/modules/products/typeorm/repositories/ProductRepository.ts
@@ -0,0 +1,13 @@
+import { Repository } from "typeorm";
+import Product from "../entities/Products";
+
+export default class ProductRepository 
+    extends Repository<Product>{
+        public async findByName(name: string) 
+        : Promise<Product | undefined>{
+            const product = await this.findOne({
+                where: { name }
+            })
+            return product;
+        }
+}
